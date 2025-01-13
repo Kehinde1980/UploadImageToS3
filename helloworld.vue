a423c5ed758e45296d06dd10bc0736a4e376e405
Kehinde1980 <144909364+Kehinde1980@users.noreply.github.com>
2025-01-13 21:48:10 +0000

    Update helloworld.vue

--- helloworld.vue
+++ helloworld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://cxmhw66lzb.execute-api.us-east-1.amazonaws.com/Dev-Env'
+const API_ENDPOINT = 'https://6pkos688mj.execute-api.us-east-1.amazonaws.com/default/project2severless'
 
 export default {
   name: 'app',
